e8c3db672a68475d9284b7c1efca67a711bd8db1
Jingyu <jhuang664@wisc.edu>
2025-11-24 19:47:54 -0600

    update

--- run.bash
+++ run.bash
@@ -34,7 +34,7 @@ PYTHONUNBUFFERED=1 python -m verl.trainer.main_ppo \
   actor_rollout_ref.rollout.n=5 \
   actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
   actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
-  actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+  actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
   \
   actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
   actor_rollout_ref.ref.fsdp_config.param_offload=True \
