fc03481e8949f79b14e437701d5d5c02b61c3e9b
Jingyu <jhuang664@wisc.edu>
2025-11-24 20:41:51 -0600

    update

--- run.bash
+++ run.bash
@@ -2,6 +2,7 @@
 set -e
 export VLLM_USE_FLASHINFER_SAMPLER=0
 export CUDA_VISIBLE_DEVICES=0,1
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # ====== 路径自己改 ======
 TRAIN_FILE=$PWD/data/vcbench/train.parquet
 TEST_FILE=$PWD/data/vcbench/test.parquet
@@ -13,7 +14,7 @@ REWARD_NAME=compute_score            # 你 reward 里的函数名
 PYTHONUNBUFFERED=1 python -m verl.trainer.main_ppo \
   data.train_files=$TRAIN_FILE \
   data.val_files=$TEST_FILE \
-  data.train_batch_size=256 \
+  data.train_batch_size=8 \
   data.max_prompt_length=1024 \
   data.max_response_length=256 \
   data.filter_overlong_prompts=True \
@@ -24,21 +25,24 @@ PYTHONUNBUFFERED=1 python -m verl.trainer.main_ppo \
   actor_rollout_ref.model.enable_gradient_checkpointing=True \
   \
   actor_rollout_ref.actor.optim.lr=1e-6 \
-  actor_rollout_ref.actor.ppo_mini_batch_size=64 \
-  actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
+  actor_rollout_ref.actor.ppo_mini_batch_size=4 \
+  actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
   actor_rollout_ref.actor.use_kl_loss=True \
   actor_rollout_ref.actor.kl_loss_coef=0.001 \
   actor_rollout_ref.actor.kl_loss_type=low_var_kl \
   actor_rollout_ref.actor.entropy_coeff=0 \
   \
   actor_rollout_ref.rollout.name=vllm \
-  actor_rollout_ref.rollout.n=5 \
-  actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
+  actor_rollout_ref.rollout.n=8 \
+  actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
   actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
   actor_rollout_ref.rollout.gpu_memory_utilization=0.35 \
   \
-  actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
+  actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
   actor_rollout_ref.ref.fsdp_config.param_offload=True \
+  \
+  actor_rollout_ref.actor.fsdp_config.param_offload=True \
+  actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
   \
   algorithm.adv_estimator=grpo \
   algorithm.use_kl_in_reward=False \
